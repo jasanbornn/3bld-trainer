5ea456c56c66926e10846d12f95c4ab779696346
Jamie Sanborn <jamieosanborn@gmail.com>
2026-03-22 15:31:37 -0500

    Added scramble visualizer

--- src/App.jsx
+++ src/App.jsx
@@ -17,27 +17,111 @@ function Button({text, onClick}) {
 
 }
 
+function CubeNet({stickers}) {
+
+    return (
+        <div id="cube-net">
+            <CubeNetSpacer/>
+            <CubeNetFace stickers={stickers.substring(0,9)}/>
+            <CubeNetSpacer/>
+            <CubeNetSpacer/>
+
+            <CubeNetFace stickers={stickers.substring(36,45)}/>
+            <CubeNetFace stickers={stickers.substring(18,27)}/>
+            <CubeNetFace stickers={stickers.substring(9,18)}/>
+            <CubeNetFace stickers={stickers.substring(45,54)}/>
+
+            <CubeNetSpacer/>
+            <CubeNetFace stickers={stickers.substring(27,36)}/>
+            <CubeNetSpacer/>
+            <CubeNetSpacer/>
+        </div>
+    );
+}
+
+function CubeNetFace({stickers}) {
+
+    function getColorFromSticker(sticker) {
+        switch(sticker) {
+            case "U":
+                return "white";
+            case "D":
+                return "yellow";
+            case "L":
+                return "orange";
+            case "R":
+                return "red";
+            case "F":
+                return "green";
+            case "B":
+                return "blue";
+            default:
+                return "pink";
+        }
+    }
+
+    return (
+        <div id="cube-net-face">
+            <CubeNetSticker color={getColorFromSticker(stickers.charAt(0))}/>
+            <CubeNetSticker color={getColorFromSticker(stickers.charAt(1))}/>
+            <CubeNetSticker color={getColorFromSticker(stickers.charAt(2))}/>
+
+            <CubeNetSticker color={getColorFromSticker(stickers.charAt(3))}/>
+            <CubeNetSticker color={getColorFromSticker(stickers.charAt(4))}/>
+            <CubeNetSticker color={getColorFromSticker(stickers.charAt(5))}/>
+
+            <CubeNetSticker color={getColorFromSticker(stickers.charAt(6))}/>
+            <CubeNetSticker color={getColorFromSticker(stickers.charAt(7))}/>
+            <CubeNetSticker color={getColorFromSticker(stickers.charAt(8))}/>
+        </div>
+    );
+}
+
+
+function CubeNetSticker({color}) {
+
+    return (
+        <div id="cube-net-sticker" style={{backgroundColor: color}}>
+
+        </div>
+    )
+}
+
+function CubeNetSpacer() {
+
+    return (
+        <div id="cube-net-spacer">
+
+        </div>
+    );
+}
+
 function App() {
-    const [count, setCount] = useState(0);
     const [solution, setSolution] = useState("");
+    const [stickers, setStickers] = useState("RDFDUUBUULRLLRFLFRLBBBFFFBFRLULDRURDFLDBLRRUDDFUUBDBDB");
 
     function updateScramble() {
         cube.randomize();
         setSolution(Cube.inverse(cube.solve()));
+        setStickers(cube.asString());
         console.log("state: " + JSON.stringify(cube.toJSON()));
         console.log("faces: " + cube.asString());
     }
 
     return (
         <>
-        <section id="scramble-box">
-        <div>
-            <h1>{solution}</h1>
+        <div id="scramble-container">
+            <div>
+                <h1>{solution}</h1>
+            </div>
+            <Button text="↺" onClick={updateScramble}/>
         </div>
 
-        <Button text="↺" onClick={updateScramble}/>
+        <div id="cube-net-container">
+            <CubeNet stickers={stickers}/>
+        </div>
 
-        </section>
+
 
         </>
     )
