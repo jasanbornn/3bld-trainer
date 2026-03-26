dc2e23757729b20b0a8c5ac904b4a790cbc90d20
Jamie Sanborn <jamieosanborn@gmail.com>
2026-03-25 20:04:15 -0500

    Added corner memo generator

--- src/App.jsx
+++ src/App.jsx
@@ -96,7 +96,17 @@ function CubeNetSpacer() {
     );
 }
 
+
 function MemoPairs({cubeState}) {
+    return (
+        <>
+            <EdgeMemoPairs cubeState={cubeState}/>
+            <CornerMemoPairs cubeState={cubeState}/>
+        </>
+    );
+}
+
+function EdgeMemoPairs({cubeState}) {
     //buffer edge (DF) letter K: perm: 5 orientation: 0
     //buffer corner (UFB) letter A: perm: 2 orientation 0
 
@@ -192,17 +202,13 @@ function MemoPairs({cubeState}) {
 
     function getNextEdgeLabel(label) {
         const [perm, orient] = getEdgePandOFromLabel(label);
-
         const nextPerm = cubeState.ep[perm];
-
         const nextOrient = orient ? !cubeState.eo[perm] : cubeState.eo[perm];
-        
         const nextLabel = getEdgeLabelFromPandO(nextPerm, nextOrient);
-
         return nextLabel;
     }
 
-    function getSpeffzLetterFromLabel(label) {
+    function getSpeffzLetterFromEdgeLabel(label) {
         const letterScheme = "ABCDEFGHIJKLMNOPQRSTUVWX"
 
         switch(label) {
@@ -261,7 +267,7 @@ function MemoPairs({cubeState}) {
         let edgeMemoString = "";
 
         for(let i = 0; i < edgeMemo.length; i++) {
-            edgeMemoString += getSpeffzLetterFromLabel(edgeMemo[i]);
+            edgeMemoString += getSpeffzLetterFromEdgeLabel(edgeMemo[i]);
             if(i % 2 == 1) {
                 edgeMemoString += " ";
             }
@@ -290,8 +296,6 @@ function MemoPairs({cubeState}) {
             }
         }
 
-        console.log("presolved edges: " + solvedEdges);
-
         let currentLabel = bufferLabel;
         for(let i = 0; i < solvedEdges.length; i++) {
             if(solvedEdges[i] == 0) {
@@ -300,9 +304,8 @@ function MemoPairs({cubeState}) {
                 let nextLabel = getNextEdgeLabel(currentLabel);
 
                 if(nextLabel == bufferLabel || nextLabel == bufferLabelFlipped) {
-                    //bufferIndex = getEdgePandOFromLabel(bufferLabel)[0];
                     for(let j = 0; j < solvedEdges.length; j++) {
-                        if(solvedEdges[j] == 0 /*&& j != bufferIndex*/) {
+                        if(solvedEdges[j] == 0) {
                             const nextPerm = j;
                             const nextOrient = 0;
                             nextLabel = getEdgeLabelFromPandO(nextPerm, nextOrient);
@@ -314,11 +317,8 @@ function MemoPairs({cubeState}) {
                             const edgeIndex = getEdgePandOFromLabel(nextNextLabel)[0];
                             solvedEdges[edgeIndex] = 1;
                             edgeMemo.push(nextNextLabel);
-
                             currentLabel = nextNextLabel;
-                            
-                            console.log("edgeMemo: " + edgeMemo);
-                            console.log("new cycle: " + nextLabel);
+
                             break;
                         }
                     }
@@ -326,20 +326,16 @@ function MemoPairs({cubeState}) {
                     const edgeIndex = getEdgePandOFromLabel(nextLabel)[0];
                     solvedEdges[edgeIndex] = 1;
                     edgeMemo.push(nextLabel);
-                    console.log("edgeMemo: " + edgeMemo);
-                    console.log("solved edges: " + solvedEdges);
                     currentLabel = nextLabel;
                 }
 
                 //failsafe
                 if(edgeMemo.length > 30) {
-                    console.log("edge memo failed: " + edgeMemo);
                     return ("edge memo failed " + edgeMemo.toString());
                 }
             }
         }
 
-        console.log("final edge memo: " + edgeMemo);
         return edgeMemoToString(edgeMemo);
     }
 
@@ -347,6 +343,300 @@ function MemoPairs({cubeState}) {
     return (
         <div id="edge-memo-text">
             <h1>{genEdgeMemo()}</h1>
+            <h3>Edge Memo</h3>
+        </div>
+    )
+}
+
+function CornerMemoPairs({cubeState}) {
+    
+
+    function getCornerLabelFromPandO(permutation, orientation) {
+        switch(permutation) {
+            case 0:
+                switch(orientation) {
+                    case 0:
+                        return "UFR";
+                    case 1:
+                        return "FUR";
+                    case 2:
+                        return "RUF";
+                }
+            case 1:
+                switch(orientation) {
+                    case 0:
+                        return "UFL";
+                    case 1:
+                        return "LUF";
+                    case 2:
+                        return "FUL";
+                }
+            case 2:
+                switch(orientation) {
+                    case 0:
+                        return "UBL";
+                    case 1:
+                        return "BUL";
+                    case 2:
+                        return "LUB";
+                }
+            case 3:
+                switch(orientation) {
+                    case 0:
+                        return "UBR";
+                    case 1:
+                        return "RUB";
+                    case 2:
+                        return "BUR";
+                }
+            case 4:
+                switch(orientation) {
+                    case 0:
+                        return "DFR";
+                    case 1:
+                        return "RDF";
+                    case 2:
+                        return "FDR";
+                }
+            case 5:
+                switch(orientation) {
+                    case 0:
+                        return "DFL";
+                    case 1:
+                        return "FDL";
+                    case 2:
+                        return "LDF";
+                }
+            case 6:
+                switch(orientation) {
+                    case 0:
+                        return "DBL";
+                    case 1:
+                        return "LDB";
+                    case 2:
+                        return "BDL";
+                }
+            case 7:
+                switch(orientation) {
+                    case 0:
+                        return "DBR";
+                    case 1:
+                        return "BDR";
+                    case 2:
+                        return "RDB";
+                }
+
+        }
+    }
+
+    function getCornerPandOFromLabel(label) {
+        switch(label) {
+            case "UFR":
+                return [0, 0];
+            case "FUR":
+                return [0, 1];
+            case "RUF":
+                return [0, 2];
+            case "UFL":
+                return [1, 0];
+            case "LUF":
+                return [1, 1];
+            case "FUL":
+                return [1, 2];
+            case "UBL":
+                return [2, 0];
+            case "BUL":
+                return [2, 1];
+            case "LUB":
+                return [2, 2];
+            case "UBR":
+                return [3, 0];
+            case "RUB":
+                return [3, 1];
+            case "BUR":
+                return [3, 2];
+            case "DFR":
+                return [4, 0];
+            case "RDF":
+                return [4, 1];
+            case "FDR":
+                return [4, 2];
+            case "DFL":
+                return [5, 0];
+            case "FDL":
+                return [5, 1];
+            case "LDF":
+                return [5, 2];
+            case "DBL":
+                return [6, 0];
+            case "LDB":
+                return [6, 1];
+            case "BDL":
+                return [6, 2];
+            case "DBR":
+                return [7, 0];
+            case "BDR":
+                return [7, 1];
+            case "RDB":
+                return [7, 2];
+        }
+    }
+
+    function getSpeffzLetterFromCornerLabel(label) {
+        const letterScheme = "ABCDEFGHIJKLMNOPQRSTUVWX"
+
+        switch(label) {
+            case "UBL":
+                return letterScheme[0];
+            case "UBR":
+                return letterScheme[1];
+            case "UFR":
+                return letterScheme[2];
+            case "UFL":
+                return letterScheme[3];
+            case "LUB":
+                return letterScheme[4];
+            case "LUF":
+                return letterScheme[5];
+            case "LDF":
+                return letterScheme[6];
+            case "LDB":
+                return letterScheme[7];
+            case "FUL":
+                return letterScheme[8];
+            case "FUR":
+                return letterScheme[9];
+            case "FDR":
+                return letterScheme[10];
+            case "FDL":
+                return letterScheme[11];
+            case "RUF":
+                return letterScheme[12];
+            case "RUB":
+                return letterScheme[13];
+            case "RDB":
+                return letterScheme[14];
+            case "RDF":
+                return letterScheme[15];
+            case "BUR":
+                return letterScheme[16];
+            case "BUL":
+                return letterScheme[17];
+            case "BDL":
+                return letterScheme[18];
+            case "BDR":
+                return letterScheme[19];
+            case "DFL":
+                return letterScheme[20];
+            case "DFR":
+                return letterScheme[21];
+            case "DBR":
+                return letterScheme[22];
+            case "DBL":
+                return letterScheme[23];
+        }
+
+    }
+
+    function cornerMemoToString(cornerMemo) {
+        let cornerMemoString = "";
+
+        for(let i = 0; i < cornerMemo.length; i++) {
+            cornerMemoString += getSpeffzLetterFromCornerLabel(cornerMemo[i]);
+            if(i % 2 == 1) {
+                cornerMemoString += " ";
+            }
+        }
+
+        return cornerMemoString;
+
+    }
+
+
+    function getNextCornerLabel(label) {
+        const [perm, orient] = getCornerPandOFromLabel(label);
+        const nextPerm = cubeState.cp[perm];
+        const nextOrient = (orient + cubeState.co[perm]) % 3;
+        const nextLabel = getCornerLabelFromPandO(nextPerm, nextOrient);
+        return nextLabel;
+    }
+
+    function getTwistedCornerLabel(label, twistAmount) {
+        const [perm, orient] = getCornerPandOFromLabel(label);
+        const newOrient = (orient + twistAmount) % 3;
+        const twisted = getCornerLabelFromPandO(perm, newOrient);
+        return twisted;
+    }
+
+    function genCornerMemo() {
+        let bufferLabel = "UBL";
+        let bufferLabelTwistBy1 = "LUB";
+        let bufferLabelTwistBy2 = "BUL";
+        let cornerMemo = [];
+        let solvedCorners = [0, 0, 0, 0, 0, 0, 0, 0];
+
+        //buffer position is considered solved at the start
+        let bufferIndex = getCornerPandOFromLabel(bufferLabel)[0];
+        solvedCorners[bufferIndex] = 1;
+
+        //find solved corners
+        for(let i = 0; i < cubeState.cp.length; i++) {
+            if(cubeState.cp[i] == i) {
+                if(cubeState.co[i] == 0) {
+                    solvedCorners[i] = 1;
+                }
+            }
+        }
+
+        let currentLabel = bufferLabel;
+        for(let i = 0; i < solvedCorners.length; i++) {
+            if(solvedCorners[i] == 0) {
+                i = -1;
+
+                let nextLabel = getNextCornerLabel(currentLabel);
+
+                if(nextLabel == bufferLabel || nextLabel == bufferLabelTwistBy1 || nextLabel == bufferLabelTwistBy2) {
+                    for(let j = 0; j < solvedCorners.length; j++) {
+                        if(solvedCorners[j] == 0) {
+                            const nextPerm = j;
+                            const nextOrient = 0;
+                            nextLabel = getCornerLabelFromPandO(nextPerm, nextOrient);
+                            cornerMemo.push(nextLabel);
+                            let nextNextLabel = getNextCornerLabel(nextLabel);
+                            bufferLabel = nextNextLabel;
+                            bufferLabelTwistBy1 = getTwistedCornerLabel(bufferLabel, 1);
+                            bufferLabelTwistBy2 = getTwistedCornerLabel(bufferLabel, 2);
+
+                            const cornerIndex = getCornerPandOFromLabel(nextNextLabel)[0];
+                            solvedCorners[cornerIndex] = 1;
+                            cornerMemo.push(nextNextLabel);
+                            currentLabel = nextNextLabel;
+
+                            break;
+                        }
+                    }
+                } else {
+                    const cornerIndex = getCornerPandOFromLabel(nextLabel)[0];
+                    solvedCorners[cornerIndex] = 1;
+                    cornerMemo.push(nextLabel);
+                    currentLabel = nextLabel;
+                }
+
+                //failsafe
+                if(cornerMemo.length > 20) {
+                    return ("corner memo failed " + cornerMemo.toString());
+                }
+            }
+        }
+
+        return cornerMemoToString(cornerMemo);
+    }
+
+
+    return (
+        <div id="corner-memo-text">
+            <h1>{genCornerMemo()}</h1>
+            <h3>Corner Memo</h3>
         </div>
     )
 }
