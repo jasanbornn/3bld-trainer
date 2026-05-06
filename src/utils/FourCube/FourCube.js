112857c3ebc628ae99cc5f30c74eaa37753f787d
Jamie Sanborn <jamieosanborn@gmail.com>
2026-05-05 21:01:47 -0500

    Start implementation of 4x4 'FourCube'

---
+++ src/utils/FourCube/FourCube.js
@@ -0,0 +1,23 @@
+function FourCube() {
+    //Centers
+    //inner UpBackLeft, etc.
+    //Speffz order
+    let iUBL, iUBR, iUFR, iUFL, iLUB, iLUF, iLDF, iLDB, iFUL, iFUR, iFDR, iFDL, iRUF, iRUB, iRDB, iRDF, iBUR, iBUL, iBDL, iBDR, iDFL, iDFR, iDBR, iDBL;
+
+    [iUBL, iUBR, iUFR, iUFL, iLUB, iLUF, iLDF, iLDB, iFUL, iFUR, iFDR, iFDL, iRUF, iRUB, iRDB, iRDF, iBUR, iBUL, iBDL, iBDR, iDFL, iDFR, iDBR, iDBL] = 
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
+
+    //Corner
+    //UpBackLeft, etc.
+    //Speffz order
+    let UBL, UBR, UFR, UFL, DFL, DFR, DBR, DBL;
+
+    [UBL, UBR, UFR, UFL, DFL, DFR, DBR, DBL] = [0, 1, 2, 3, 4, 5, 6, 7]
+
+    //Edges
+    //UpBack, etc.
+    //Speffz order
+    let UB, UR, UF, UL, LU, LF, LD, LB, FU, FR, FD, FL, RU, RB, RD, RF, BU, BL, BD, BR, DF, DR, DB, DL;
+    [UB, UR, UF, UL, LU, LF, LD, LB, FU, FR, FD, FL, RU, RB, RD, RF, BU, BL, BD, BR, DF, DR, DB, DL] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+    
+}
